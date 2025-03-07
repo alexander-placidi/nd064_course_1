6516cbadac6ae815eff028e58d2f78cc510ab724
Alexander Placidi <a.placidi@posteo.de>
2025-03-07 11:00:08 +0100

    health check

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -4,6 +4,15 @@ import logging
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+class DatabaseException(Exception):
+    pass
+
+def get_json_response(body, statuscode):
+    return app.response_class(
+        response=json.dumps(body),
+        status=statuscode,
+        mimetype='application/json'
+    )
 
 def counted(func):
     def counter_func(*x):
@@ -17,10 +26,13 @@ def counted(func):
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    return connection
-
+    try:
+        connection = sqlite3.connect('database.db')
+        connection.row_factory = sqlite3.Row
+        return connection
+    except Exception as ex:
+        print(ex)
+        raise DatabaseException("Database connection failed!")
 
 # Function to get a post using its ID
 @counted
@@ -94,14 +106,23 @@ def create():
 @app.route('/healthz')
 def health():
     body = {}
-    body["result"] = "OK - healthy"
+    
+    try:
+        if not posts_table_exists():
+            raise DatabaseException("Database not initialized!")
+    except Exception as ex:
+        logging.error(ex)
+        body["result"] = "Error - unhealthy"
+        return get_json_response(body,500) 
 
-    response = app.response_class(
-        response=json.dumps(body),
-        status=200,
-        mimetype='application/json'
-    )
-    return response 
+    body["result"] = "OK - healthy"
+    return get_json_response(body,200) 
+
+def posts_table_exists():
+    connection = get_db_connection()
+    row_count = len(connection.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='posts'").fetchall())
+    connection.close()
+    return True if row_count > 0 else False
 
 @app.route('/metrics')
 def metrics():
